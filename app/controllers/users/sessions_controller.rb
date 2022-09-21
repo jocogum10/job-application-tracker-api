c0128c2e05876773b028cfdeaad46163fac143cb
Carlos Joco Gumanay <jocogum@DESKTOP-15283M7.localdomain>
2022-09-21 21:06:39 +0800

    config devise-jwt for users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(_resource, _opts = {})
     render json: {
-      message: "You are logged in.",
+      message: "You are logged in...",
       user: current_user
     }, status: :ok
   end
@@ -25,7 +25,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def log_out_failure
     render json: {
-      message: "Hmm nothing happened.",
+      message: "Hmm nothing happened...",
       status: :unauthorized
     }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
 
   get '/member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
