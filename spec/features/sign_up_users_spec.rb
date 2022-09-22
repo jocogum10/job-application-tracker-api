b89216ee6ce54eaed6a24090588fe0707cc363e2
Carlos Joco Gumanay <jocogum@DESKTOP-15283M7.localdomain>
2022-09-22 21:21:35 +0800

    add rspec test for users sign up

---
+++ spec/features/sign_up_users_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "get all questions route", :type => :request do
+  before do 
+      post '/users' , params: {
+      email: "test1@test.com",
+      password: "password",
+      confirmation_password: "password"
+    }
+  end
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'returns a message for successful sign up' do
+    expect(JSON.parse(response.body)['message']).to eq('Signed up successfully.')
+  end
+end
